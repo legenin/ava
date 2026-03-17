75575cde5505c0562342856a6c6428c62f6e9c53
Aleksandr Legenin <legenin.av@rs-class.org>
2026-03-17 11:39:36 +0300

    Add model

---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+namespace ava.Models;
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsFictitious { get; set; }
+
+    public Person(string firstName , string lastName, bool isFictitious)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        IsFictitious = isFictitious;
+    }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
